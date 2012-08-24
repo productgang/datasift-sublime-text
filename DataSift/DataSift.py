b43b0f1578d69e3beb74243e066a464ccf33d4d2
Stuart Dallas <github@3ft9.com>
2012-08-24 15:51:55 +0100

    Switched to use the official Python client library, and added the ability to validate the syntax without generating the hash.

--- DataSift/DataSift.py
+++ DataSift/DataSift.py
@@ -1,33 +1,46 @@
-import sublime, sublime_plugin, urllib, urllib2, json
+import sublime, sublime_plugin, sys, os
+sys.path[0:0] = [os.path.join(os.path.dirname(__file__), "lib"),]
+import datasift
+
+def get_datasift_user(settings):
+    api_name = settings.get('datasift_api_name')
+    api_key = settings.get('datasift_api_key')
+    if not api_name or not api_key:
+        return False
+    return datasift.User(api_name, api_key)
+
+class DatasiftValidateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        csdl = self.view.substr(sublime.Region(0, self.view.size()))
+        try:
+            datasift_user = get_datasift_user(self.view.settings())
+            if not datasift_user:
+                sublime.error_message('%s: Username and/or API key not set!' % (__name__))
+            else:
+                datasift_user.create_definition(csdl).validate()
+                sublime.message_dialog('%s: CSDL validated successfully' % (__name__))
+        except datasift.InvalidDataError as e:
+            sublime.error_message(str(e))
+        except datasift.CompileFailedError as e:
+            sublime.error_message('Validation failed: %s' % str(e))
+        except datasift.APIError as (e, c):
+            sublime.error_message(str(e))
 
 class DatasiftCompileCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		csdl = self.view.substr(sublime.Region(0, self.view.size()))
-		api_name = self.view.settings().get('datasift_api_name')
-		api_key = self.view.settings().get('datasift_api_key')
-		validate_endpoint = self.view.settings().get('datasift_validate_endpoint', 'http://api.datasift.com/validate?%s')
-		compile_endpoint = self.view.settings().get('datasift_compile_url', 'http://api.datasift.com/compile?%s')
-		if not api_name or not api_key:
-			sublime.error_message('%s: Username and/or API key not set!' % (__name__))
-			return
-		options_encoded = urllib.urlencode({
-			'csdl': csdl,
-			'username': api_name,
-			'api_key': api_key
-		})
-		try:
-			http_file = urllib2.urlopen(compile_endpoint%options_encoded)
-			result = json.loads(http_file.read())
-			sublime.set_clipboard(result['hash'])
-			sublime.message_dialog('%s: CSDL compiled successfully, hash %s has been copied into your clipboard.' % (__name__, result['hash']))
-		except (urllib2.HTTPError) as (e):
-			err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
-			if e.code == 401:
-				err = '%s: Authorization failed!' % (__name__)
-			if e.code == 400:
-				print e
-				err = '%s: CSDL validation failed!' % (__name__)
-			sublime.error_message(err)
-		except (urllib2.URLError) as (e):
-			err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
-			sublime.error_message(err)
+    def run(self, edit):
+        csdl = self.view.substr(sublime.Region(0, self.view.size()))
+        try:
+            datasift_user = get_datasift_user(self.view.settings())
+            if not datasift_user:
+                sublime.error_message('%s: Username and/or API key not set!' % (__name__))
+            else:
+                definition = datasift_user.create_definition(csdl)
+                stream_hash = definition.get_hash()
+                sublime.set_clipboard(stream_hash)
+                sublime.message_dialog('%s: CSDL compiled successfully, hash %s has been copied into your clipboard.' % (__name__, stream_hash))
+        except datasift.InvalidDataError as e:
+            sublime.error_message(str(e))
+        except datasift.CompileFailedError as e:
+            sublime.error_message('Compilation failed: %s' % str(e))
+        except datasift.APIError as (e, c):
+            sublime.error_message(str(e))
