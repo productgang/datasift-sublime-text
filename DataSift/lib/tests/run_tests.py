fb91e152a0cd0b5b09ddedde4d7290bfb9cb48d0
Stuart Dallas <github@3ft9.com>
2012-08-24 19:01:16 +0100

    Added the client library back in.

---
+++ DataSift/lib/tests/run_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import unittest, os, glob
+
+# Import the tests
+from test_user import TestUser
+from test_definition import TestDefinition
+from test_http_stream import TestHttpStreamErrors
+
+# Run the tests
+unittest.main()
