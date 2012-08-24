a6a3211bdeb0f79140bb9d7d53c0492ad569b2eb
Lukas Klein <lukas@lukasklein.com>
2012-08-24 12:46:04 +0100

    Initial commit

---
+++ DataSift/DataSift.py
@@ -0,0 +1,35 @@
+import sublime, sublime_plugin, urllib, urllib2, json
+
+class DatasiftCompileCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		csdl = self.view.substr(sublime.Region(0, self.view.size()))
+		api_name = self.view.settings().get('datasift_api_name')
+		api_key = self.view.settings().get('datasift_api_key')
+		validate_endpoint = self.view.settings().get('datasift_validate_endpoint', 'http://api.datasift.com/validate?%s')
+		compile_endpoint = self.view.settings().get('datasift_compile_url', 'http://api.datasift.com/compile?%s')
+		if not api_name or not api_key:
+			sublime.error_message('%s: Username and/or API key not set!' % (__name__))
+			return
+		options_encoded = urllib.urlencode({
+			'csdl': csdl,
+			'username': api_name,
+			'api_key': api_key
+		})
+		try:
+			print validate_endpoint%options_encoded
+			http_file = urllib2.urlopen(validate_endpoint%options_encoded)
+			http_file = urllib2.urlopen(compile_endpoint%options_encoded)
+			result = json.loads(http_file.read())
+			sublime.set_clipboard(result['hash'])
+			sublime.message_dialog('%s: CSDL compiled successfully, hash %s has been copied into your clipboard.' % (__name__, result['hash']))
+		except (urllib2.HTTPError) as (e):
+			err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+			if e.code == 401:
+				err = '%s: Authorization failed!' % (__name__)
+			if e.code == 400:
+				print e
+				err = '%s: CSDL validation failed!' % (__name__)
+			sublime.error_message(err)
+		except (urllib2.URLError) as (e):
+			err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+			sublime.error_message(err)
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 datasift-sublime-text
 =====================
 
-DataSift Sublime Text Plugin
+DataSift Sublime Text Plugin to validate and compile CSDL
+
+Installation
+------------
+
+Just copy the DataSift directory into your Sublime Text packages directory (open it by going to Preferences -> Browse Packages...)
+
+To set your API username and key, add them to your user preferences.
+
+
+```javascript
+{
+	"datasift_api_name": "development",
+	"datasift_api_key": "yourapikey"
+}
+```
+
+
+Usage
+-----
+
+To compile CSDL code just hit Cmd + Shift + C (Ctrl + Shift + C on Windows / Linux) or alternatively go to Tools -> Datasift Compile. If your coda validates it will automatically copy the stream hash into your clipboard. You can change the keyboard mapping in the .sublime-keymap files.
